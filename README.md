f5caafd1e38a1d011a27515ade168be84a20be1f
Jesse0502 <jas0502singh@gmail.com>
2022-06-20 15:35:42 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+curl http://localhost:3000/web_model/model.json
+yarn start
---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React, { useEffect, useState } from "react";
+import "./styles.css";
+import Yolo from "./Yolo";
+
+const App = () => {
+  return <Yolo />;
+};
+
+export default App;
